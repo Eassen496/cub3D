80c3f6f9f8d707291a0575cfc40074151162e675
Eassen <ale-roux@student.42mulhouse.fr>
2023-09-20 05:04:36 +0200

    mouse

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ale-roux <ale-roux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 21:19:57 by ale-roux          #+#    #+#             */
-/*   Updated: 2023/09/20 02:31:13 by ale-roux         ###   ########.fr       */
+/*   Updated: 2023/09/20 03:02:14 by ale-roux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -292,13 +292,13 @@ int	startpos(t_cube *cube, int x, int y)
 	if (cube->angle != -1)
 		return (1);
 	if (cube->utils.map[y][x] == 'N')
-		cube->angle = 90;
+		cube->angle = 900;
 	else if (cube->utils.map[y][x] == 'S')
-		cube->angle = 270;
+		cube->angle = 2700;
 	else if (cube->utils.map[y][x] == 'E')
 		cube->angle = 0;
 	else if (cube->utils.map[y][x] == 'W')
-		cube->angle = 180;
+		cube->angle = 1800;
 	else
 		return (1);
 	cube->xpos = x * 100 + 50;
